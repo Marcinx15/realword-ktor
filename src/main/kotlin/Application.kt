a3d878851bafdf0523dc4170317d41f9fe1e552a
Marcin Kurek <marcin.kurek@schibsted.com>
2025-04-24 19:49:02 +0200

    cleanup a bit

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,14 +1,24 @@
 package com.example
 
 import io.ktor.server.application.*
+import io.ktor.server.plugins.swagger.swaggerUI
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
-    configureHTTP()
-    configureSerialization()
-    configureDatabases()
     configureRouting()
+}
+
+fun Application.configureRouting() {
+    routing {
+        swaggerUI(path = "openapi")
+        get("/whatsup") {
+            call.respondText("What's up motherfuckers?")
+        }
+    }
 }
--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -13,7 +13,7 @@ class ApplicationTest {
         application {
             module()
         }
-        client.get("/").apply {
+        client.get("/whatsup").apply {
             assertEquals(HttpStatusCode.OK, status)
         }
     }
